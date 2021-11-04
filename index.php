2759ef383d81d04b02988c913a6dfff10b0810fb
bibmode <vievenavales29@gmail.com>
2021-11-04 21:05:28 +0800

    added config, login and register pages and footer

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+//allow the config
+define('__CONFIG__', true);
+//require the config
+require_once "inc/config.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,38 +22,18 @@
 <body>
 
   <div class="uk-section uk-container">
-    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-
-      <form class="uk-form-stacked js-login">
-
-        <div class="uk-margin">
-          <label class="uk-form-label" for="form-stacked-text">Email</label>
-          <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com" required="required">
-          </div>
-        </div>
-
-        <div class="uk-margin">
-          <label class="uk-form-label" for="form-stacked-text">Password</label>
-          <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password" required="required">
-          </div>
-        </div>
-
-        <div class="uk-margin">
-          <button class="uk-button uk-button-default" type="submit">Login</button>
-        </div>
-
-
-
-      </form>
-
-    </div>
+    <?php echo "Hello world";
+    echo date("Y m d");
+    ?>
+    <p>
+      <a href="/PHP-login-registration/login.php">Login</a>
+      <a href="/PHP-login-registration/register.php">Register</a>
+    </p>
   </div>
 
-  <!-- UIkit JS -->
-  <script src="https://cdn.jsdelivr.net/npm/uikit@3.8.0/dist/js/uikit.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/uikit@3.8.0/dist/js/uikit-icons.min.js"></script>
+  <?php require_once "inc/footer.php" ?>
+
+
 </body>
 
 </html>
---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+
+//allow the config
+define('__CONFIG__', true);
+//require the config
+require_once "inc/config.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.8.0/dist/css/uikit.min.css" />
+  <title>PHP Login System</title>
+</head>
+
+<body>
+
+  <div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+
+      <form class="uk-form-stacked js-login">
+
+        <div class="uk-margin">
+          <label class="uk-form-label" for="form-stacked-text">Email</label>
+          <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com" required="required">
+          </div>
+        </div>
+
+        <div class="uk-margin">
+          <label class="uk-form-label" for="form-stacked-text">Password</label>
+          <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password" required="required">
+          </div>
+        </div>
+
+        <div class="uk-margin">
+          <button class="uk-button uk-button-default" type="submit">Login</button>
+        </div>
+
+
+
+      </form>
+
+    </div>
+  </div>
+
+  <?php require_once "inc/footer.php" ?>
+
+
+</body>
+
+</html>
